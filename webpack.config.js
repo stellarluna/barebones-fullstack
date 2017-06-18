f32783962423f59c2586d4f151fa834451797d15
Justine Wang <justinelhwang@gmail.com>
2017-06-18 16:04:23 -0700

    Import react libraries and attempt webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
